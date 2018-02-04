faee13e037ee2d58f2f1d9d1abc9e3bf08bf8201
el-davo <david.ahern26@gmail.com>
2018-02-04 20:51:05 +0000

    Fixing lint

--- server/models/event.js
+++ server/models/event.js
@@ -1,8 +1,7 @@
 'use strict';
 
 module.exports = Event => {
-
-  Event.createOptionsFromRemotingContext = function (ctx) {
+  Event.createOptionsFromRemotingContext = function(ctx) {
     let base = this.base.createOptionsFromRemotingContext(ctx);
     base.userId = ctx.req.user.userId;
     base.appId = ctx.req.user.appId;
--- server/server.js
+++ server/server.js
@@ -4,10 +4,8 @@ let loopback = require('loopback');
 let boot = require('loopback-boot');
 
 let app = module.exports = loopback();
-
-app.start = function () {
-  // start the web server
-  return app.listen(function () {
+app.start = () => {
+  return app.listen(() => {
     app.emit('started');
     let baseUrl = app.get('url').replace(/\/$/, '');
     console.log('Web server listening at: %s', baseUrl);
@@ -20,7 +18,7 @@ app.start = function () {
 
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
-boot(app, __dirname, function (err) {
+boot(app, __dirname, (err) => {
   if (err) throw err;
 
   // start the server if `$ node server.js`
